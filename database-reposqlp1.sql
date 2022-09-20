9b276c1b27d2d722cb45da33b4c029644f056d8a
Henningham22 <Henningham22@hotmail.com>
2022-09-20 16:08:45 +0100

    database1

---
+++ database-reposqlp1.sql
@@ -0,0 +1,11 @@
+create database qa_restaurant;
+
+use qa_restaurant;
+
+create table customers(cust_id int not null auto_increment,
+cust_name varchar(100) not null,
+phone_number char(11) not null,primary key(cust_id));
+
+show tables;
+
+describe customers;
